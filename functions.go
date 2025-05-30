8704348f5aca2cdc53c49a1ff8dbddcad297b007
Jian.Song@cn.ca <jian.song@cn.ca>
2025-05-30 19:55:52 -0400

    trouble shooting

--- functions.go
+++ functions.go
@@ -781,29 +781,31 @@ func jpfSortBy(arguments []interface{}) (interface{}, error) {
 	}
 }
 func jpfJoin(arguments []interface{}) (interface{}, error) {
-	fmt.Println("!!!!!!!!!Into jpfJoin !!!!!!!!!")
-	sep, ok := arguments[0].(string)
-	if !ok {
-		return nil, fmt.Errorf("first argument to join must be a string")
-	}
-	fmt.Println("!!!!!!!!! jpfJoin first argument check passed!!!!!!!!!")
-	// Accept any array and coerce items to string
-	array, ok := arguments[1].([]interface{})
-	if !ok {
-		return nil, fmt.Errorf("second argument to join must be an array")
-	}
-	fmt.Println("!!!!!!!!! jpfJoin second argument check passed!!!!!!!!!")
-	var arrayStr []string
-	for _, item := range array {
-		if item == nil {
-			arrayStr = append(arrayStr, "") // Optional: or skip or error
-		} else {
-			arrayStr = append(arrayStr, fmt.Sprintf("%v", item))
+	panic("🔥 REACHED CUSTOM jpfJoin 🔥")
+	/*
+		fmt.Println("!!!!!!!!!Into jpfJoin !!!!!!!!!")
+		sep, ok := arguments[0].(string)
+		if !ok {
+			return nil, fmt.Errorf("first argument to join must be a string")
 		}
-	}
+		fmt.Println("!!!!!!!!! jpfJoin first argument check passed!!!!!!!!!")
+		// Accept any array and coerce items to string
+		array, ok := arguments[1].([]interface{})
+		if !ok {
+			return nil, fmt.Errorf("second argument to join must be an array")
+		}
+		fmt.Println("!!!!!!!!! jpfJoin second argument check passed!!!!!!!!!")
+		var arrayStr []string
+		for _, item := range array {
+			if item == nil {
+				arrayStr = append(arrayStr, "") // Optional: or skip or error
+			} else {
+				arrayStr = append(arrayStr, fmt.Sprintf("%v", item))
+			}
+		}
 
-	fmt.Println("!!!!!!!!!jpfJoin join completed!!!!!!!!!")
-	return strings.Join(arrayStr, sep), nil
+		fmt.Println("!!!!!!!!!jpfJoin join completed!!!!!!!!!")
+		return strings.Join(arrayStr, sep), nil */
 }
 func jpfReverse(arguments []interface{}) (interface{}, error) {
 	if s, ok := arguments[0].(string); ok {
