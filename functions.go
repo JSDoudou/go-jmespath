04e48babe8dbf1ec111a56b77d74a17a6c5507ca
Jian.Song@cn.ca <jian.song@cn.ca>
2025-05-30 20:37:24 -0400

    add panic

--- functions.go
+++ functions.go
@@ -48,7 +48,7 @@ type byExprString struct {
 }
 
 func init() {
-	fmt.Println("✅ LOADED seek-ret forked go-jmespath v0.4.3")
+	fmt.Println("✅ LOADED seek-ret forked go-jmespath v0.4.4")
 }
 func (a *byExprString) Len() int {
 	return len(a.items)
